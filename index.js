e3e7c558dac594e60e5ec0f60fd59d29a754d827
abdulhakeem1232 <hakeemforgit@gmail.com>
2023-11-02 14:04:58 +0530

    done with category and sub category

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const adminRouter=require('./server/routes/admin_router.js')
 
 
 const app=express();
-const port=9000;
+const port=5500;
 
 app.use(function (req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
--- server/routes/admin_router.js
+++ server/routes/admin_router.js
@@ -16,6 +16,18 @@ router.get('/update/:email',controller.userupdate)
 router.post('/searchUser',controller.searchUser)
 router.get('/searchview',controller.searchview)
 router.get('/filter/:option',controller.filter)
+router.get('/category',controller.category)
+router.get('/newcat',controller.newcat)
+router.post('/add-category',controller.addcategory)
+router.get('/updatecat/:id',controller.updatecat)
+router.post('/update-category/:id',controller.updatecategory)
+router.get('/subcategory',controller.subcategory)
+router.get('/newsubcat',controller.newsubcat)
+router.post('/add-subcategory',controller.addsubcategory)
+router.get('/statuscat/:id',controller.catstatus)
+router.get('/deletesubcat/:id',controller.deletesubcat)
+router.get('/updatesubcat/:id',controller.updatesubcat)
+router.post('/update-subcategory/:id',controller.updatesubcategory)
 
 
 
