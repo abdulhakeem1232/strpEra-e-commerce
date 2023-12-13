be205e9080e6b0a3e6f803687745ad9c1fd8f1d3
hakeem <hakeemforgit@gmail.com>
2023-12-13 11:49:03 +0530

    corrected image croping

--- crop.js
+++ crop.js
@@ -1,39 +1,48 @@
-const sharp=require('sharp')
-const crypto=require('crypto')
-const { error } = require('console')
+const sharp = require('sharp');
+const crypto = require('crypto');
+const fs = require('fs').promises;
 
-const processImage=async(req,res,next)=>{
-    try{
-        if(!req.files || req.files.length==0){
-            throw new Error("No file Uploaded")
+const processImage = async (req, res, next) => {
+    try {
+        if (!req.files || req.files.length === 0) {
+            throw new Error("No file uploaded");
         }
 
-        const uploadFiles=req.files;
-        const processedImages=await Promise.all(
-            uploadFiles.map(async(file)=>{
-                const randomName=crypto.randomBytes(20).toString('hex')
-                const outputPath=`uploads/${randomName}.jpg`;
+        // Ensure the 'uploads' directory exists
+        await fs.mkdir('uploads', { recursive: true });
+
+        const uploadFiles = req.files;
+        const processedImages = await Promise.all(
+            uploadFiles.map(async (file) => {
+                const randomName = crypto.randomBytes(20).toString('hex');
+                const outputPath = `uploads/${randomName}.jpg`;
+
+                const { width, height } = await sharp(file.path).metadata();
+                const size = Math.min(width, height);
 
                 await sharp(file.path)
-                .extract({ width: 300, height: 300, left: 100, top:100 }) 
+                    .extract({ width: size, height: size, left: 0, top: 0 })
                     .toFormat('jpeg')
                     .jpeg({ quality: 90 })
                     .toFile(outputPath);
 
-                const processedPath=outputPath;
+                const processedPath = outputPath;
+                console.log("Original File Name:", file.originalname);
+                console.log("Image Dimensions:", { width, height });
+                console.log("Extraction Parameters:", { width: size, height: size, left: 0, top: 0 });
                 console.log("Output Path:", outputPath);
-console.log("Processed Path:", processedPath);
+                console.log("Processed Path:", processedPath);
 
-                return processedPath
+                return processedPath;
             })
         );
 
-        req.processedImages=processedImages;
+        req.processedImages = processedImages;
         next();
-    }catch(error){
-        console.log("Error processing Image:",error);
-        res.status(500).send("Error processing Image")
+    } catch (error) {
+        console.error("Error processing image:", error);
+        res.status(500).send("Error processing image");
     }
-}
+};
 
-module.exports= {processImage:processImage,}
+module.exports = { processImage: processImage };
